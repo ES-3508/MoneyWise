0a48936314386720330837e11e3e14c58a99aec0
Induwara Fernando9 <induwarafernando9@gmail.com>
2023-10-10 13:02:23 +0530

    second commit

---
+++ src/CurrencyShopForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class ShopForm extends Component {
+  state = {
+    shopName: "",
+    address: "",
+    workingHours: "",
+    currencies: [],
+    rates: []
+  };
+
+  render() {
+    return (
+      <form>
+        {/* Form fields go here */}
+      </form>
+    );
+  }
+}
+
+export default ShopForm;
